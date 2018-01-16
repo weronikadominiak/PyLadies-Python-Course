75b4fca962a9e2b146f984a512f50beaae556654
Weronika <weronika.dominiak@hotmail.com>
2018-01-16 19:26:56 +0100

    10.9

--- 1.10/10.9.py
+++ 1.10/10.9.py
@@ -4,29 +4,33 @@
 
 storage = {'weronika': 'haslo'}
 
-login = input("Login: \n")
-
-if login in storage:
-    print("Username taken")
-else:
-    pass
+#Login in if login isn't already taken:
+while True:
+    login = input("Login: \n")
+    if login in storage:
+        print("Username taken")
+    else:
+        break
 
 password = input("Password: \n")
 
-
+#check if password is correct
 def createUser(log, passw):
-
     storage[log] = passw
     print("User created!")
     return storage
 
 createUser(login, password)
 
+#Repeat login until password and login are correct
 def loginUser():
-    login = input("Login: \n")
-    password = input("Password: \n")
-    if storage[login] == password:
-        print("Succes!")
-    else:
-        print("Wrong password or login")
+    while True:
+        login = input("Login: \n")
+        password = input("Password: \n")
+        if storage[login] == password:
+            print("Succes!")
+            break
+        else:
+            print("Wrong password or login")
+
 loginUser()
