0597e3c83c8e2f5ec2f96b65080384eb30e6d9a9
Weronika <weronika.dominiak@hotmail.com>
2018-01-16 19:09:53 +0100

    10.9

--- 1.10/10.9.py
+++ 1.10/10.9.py
@@ -1,3 +1,32 @@
 # Napisz aplikację, która będzie pozwalała założyć w niej konto posiadające login i hasło.
 # Dane użytkowników trzymaj w słowniku. Pamiętaj żeby sprawdzić czy użytkownik o danym loginie już nie istnieje.
 # Aplikacja też powinna pozwolić zalogować się wykorzystując login i hasło.
+
+storage = {'weronika': 'haslo'}
+
+login = input("Login: \n")
+
+if login in storage:
+    print("Username taken")
+else:
+    pass
+
+password = input("Password: \n")
+
+
+def createUser(log, passw):
+
+    storage[log] = passw
+    print("User created!")
+    return storage
+
+createUser(login, password)
+
+def loginUser():
+    login = input("Login: \n")
+    password = input("Password: \n")
+    if storage[login] == password:
+        print("Succes!")
+    else:
+        print("Wrong password or login")
+loginUser()
